b3a3a224a89d4108802650ad0efa8f775467cea8
zhangshun <zhangshun.lu@smartoptics.de>
2024-06-06 10:24:49 +0200

    Config todo-comments.lua
    1. use `[t` to navigate the previous Todo Comment.
    2. use `]t` to navigate the next Todo Comment.
    3. use `<leader>st` to search all Todo Comments in Telescope.

--- lua/custom/plugins/todo-comments.lua
+++ lua/custom/plugins/todo-comments.lua
@@ -1,5 +1,22 @@
 -- Highlight todo, notes, etc in comments
+-- Todo matches on any text that starts with one of your defined keywords (or alt) followed by a colon:
+--
+-- TODO: do something
+-- FIX: this should be fixed
+-- HACK: weird code warning
+
 return {
-  { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
+  'folke/todo-comments.nvim',
+  cmd = { 'TodoTelescope' },
+  event = 'VimEnter',
+  config = true,
+  -- stylua: ignore
+  keys = {
+    { "]t", function() require("todo-comments").jump_next() end, desc = "Next Todo Comment" },
+    { "[t", function() require("todo-comments").jump_prev() end, desc = "Previous Todo Comment" },
+    { "<leader>st", "<cmd>TodoTelescope<cr>", desc = "[S]earch [T]odo Comments" },
+  },
+  opt = {},
 }
+
 -- vim: ts=2 sts=2 sw=2 et
